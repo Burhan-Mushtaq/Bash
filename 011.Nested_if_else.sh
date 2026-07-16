5affbfcd63767b9cae409d952edbfefee583c65d
Burhan-Mushtaq <techbmr6@gmail.com>
2026-07-16 23:37:28 +0530

    Nested If-Else statements

---
+++ 011.Nested_if_else.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Enter First number: "
+read a
+echo "Enter Second number: "
+read b
+echo "Enter Third number: "
+read c
+
+if [ $a -gt $b ];
+then    
+    if [ $a -gt $c ]
+    then
+        echo "$a is Greatest"
+    else
+        echo "$b is Greatest"
+    fi
+else
+    if [ $b -gt $a ]
+    then
+        if [ $b -gt $c ]
+        then
+            echo "$b is Greatest"
+        else
+            echo "$c is Greatest"
+        fi
+    fi
+fi
