1d30106b157e8ea558d954fd15599435735d0e2e
Burhan-Mushtaq <techbmr6@gmail.com>
2026-07-18 22:48:05 +0530

    Break & Continue

---
+++ 015.Break_Continue.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# for i in {1..10}
+# do
+#     if [ $i -eq 5 ]
+#     then
+#         break
+#     fi
+
+#     echo $i
+# done
+
+for i in {1..10}
+do
+    if [ $i -eq 3 ]
+    then
+        continue
+    fi
+
+    echo $i
+done
